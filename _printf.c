fb6c7f4e89fe89fd518f481511d74b9c2e6e548c
Kessyboy <Kesiena.darlington@yahoo.com>
2023-09-21 14:03:57 -0700

    printf proj

--- _printf.c
+++ _printf.c
@@ -7,6 +7,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format parameters
  * Return: Printed chars
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -38,7 +39,7 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
